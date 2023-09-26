4107bbaf011c3f14e37d0a0911b8851682e56906
Jacob Handwerger <jstudyeth@gmail.com>
2023-09-26 12:42:04 -0400

    solutions

--- script.js
+++ script.js
@@ -15,3 +15,75 @@ function validCapitals(word) {
     return false;
   }
 }
+
+// 9/23/2023
+
+function addBinary(num1, num2) {
+  let result = "";
+  let carryOver = 0;
+
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+
+  while (i >= 0 || j >= 0) {
+    const digit1 = i >= 0 ? parseInt(num1[i]) : 0;
+    const digit2 = j >= 0 ? parseInt(num2[j]) : 0;
+    const sum = digit1 + digit2 + carryOver;
+    if (sum === 0 || sum === 1) {
+      result = sum + result;
+      carryOver = 0;
+    } else if (sum === 2) {
+      result = "0" + result;
+      carryOver = 1;
+    } else {
+      result = "1" + result;
+      carryOver = 1;
+    }
+    console.log("RESULT: ", result);
+    i--;
+    j--;
+  }
+  if (carryOver === 1) {
+    result = "1" + result;
+  }
+  return result;
+}
+
+// 9/24/2023
+
+function longestCommonPrefix(arr) {
+  let common = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    let j = 0;
+    while (j < arr[i].length && common[j] === arr[i][j]) {
+      j++;
+    }
+    common = common.substring(0, j);
+  }
+  return common;
+}
+
+// 9/25/2023
+
+function validPalindromeRemoval(str) {
+  for (let i = 0; i < str.length; i++) {
+    let newStr = str.replace(str[i], "");
+    if (newStr.split("").reverse().join("") === newStr) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// 9/26/2023
+
+function twoSum(arr, k) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === k) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
